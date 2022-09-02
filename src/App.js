b990db505f5303074e7ccc3d5d117dae201234b6
Abdiaziiz Ali mohamed Arrale <abdiaziizarrale@gmail.com>
2022-09-03 01:19:23 +0300

    adding Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './Components/Header';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Home from './Components/Home';
+import Footer from './Components/Footer';
 function App() {
   return (
    <div className='space-y-20'>
 <Router>
      <Header/>
      <Home/>
+     <Footer />
 </Router>
  
    </div>
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <div className="bg-gray-900 w-full text-center text-white font-sans font-semibold h-12">
+            <div className="">
+                <h1 className='p-2'>@copy Right Abdiaziiz Arrale</h1>
+            </div>
+        </div>
+    
+    </div>
+  )
+}
+
+export default Footer
